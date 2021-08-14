c7c5b35fac97ece7291e8c9c31889c1367fa777f
Rémi Bernon <rbernon@codeweavers.com>
2021-08-14 15:42:53 +0200

    winebus.sys: Move mouse and keyboard devices to unixlib.c.

--- dlls/winebus.sys/hid.c
+++ dlls/winebus.sys/hid.c
@@ -27,7 +27,7 @@
 #include "hidusage.h"
 #include "ddk/wdm.h"
 
-#include "bus.h"
+#include "unix_private.h"
 
 BOOL hid_descriptor_append(struct hid_descriptor *desc, const BYTE *buffer, SIZE_T size)
 {
