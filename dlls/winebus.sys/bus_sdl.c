99d941c605a85d052e381a6d99d236585e065c9f
Rémi Bernon <rbernon@codeweavers.com>
2021-08-15 10:38:54 +0200

    winebus.sys: Return an event from SDL bus wait on device removal.
    Instead of calling bus_unlink_hid_device or IoInvalidateDeviceRelations.

--- dlls/winebus.sys/bus_sdl.c
+++ dlls/winebus.sys/bus_sdl.c
@@ -66,6 +66,7 @@ static const WCHAR sdl_busidW[] = {'S','D','L','J','O','Y',0};
 
 static void *sdl_handle = NULL;
 static UINT quit_event = -1;
+static struct list event_queue = LIST_INIT(event_queue);
 
 #define MAKE_FUNCPTR(f) static typeof(f) * p##f = NULL
 MAKE_FUNCPTR(SDL_GetError);
@@ -732,12 +733,6 @@ static BOOL set_mapped_report_from_event(DEVICE_OBJECT *device, SDL_Event *event
     return FALSE;
 }
 
-static void try_remove_device(DEVICE_OBJECT *device)
-{
-    bus_unlink_hid_device(device);
-    IoInvalidateDeviceRelations(bus_pdo, BusRelations);
-}
-
 static void try_add_device(unsigned int index)
 {
     DWORD vid = 0, pid = 0, version = 0;
@@ -826,9 +821,7 @@ static void process_device_event(SDL_Event *event)
     else if (event->type == SDL_JOYDEVICEREMOVED)
     {
         id = ((SDL_JoyDeviceEvent *)event)->which;
-        device = bus_find_hid_device(sdl_busidW, ULongToPtr(id));
-        if (device) try_remove_device(device);
-        else WARN("failed to find device with id %d\n", id);
+        bus_event_queue_device_removed(&event_queue, sdl_busidW, ULongToPtr(id));
     }
     else if (event->type >= SDL_JOYAXISMOTION && event->type <= SDL_JOYBUTTONUP)
     {
@@ -998,17 +991,24 @@ failed:
     return STATUS_UNSUCCESSFUL;
 }
 
-NTSTATUS WINAPI sdl_bus_wait(void)
+NTSTATUS WINAPI sdl_bus_wait(void *args)
 {
+    struct bus_event **result = args;
     SDL_Event event;
+
+    /* destroy previously returned event */
+    if (*result) bus_event_destroy(*result);
+    *result = NULL;
 
     do
     {
+        if (bus_event_queue_pop(&event_queue, result)) return STATUS_PENDING;
         if (pSDL_WaitEvent(&event) != 0) process_device_event(&event);
         else WARN("SDL_WaitEvent failed: %s\n", pSDL_GetError());
     } while (event.type != quit_event);
 
     TRACE("SDL main loop exiting\n");
+    bus_event_queue_destroy(&event_queue);
     dlclose(sdl_handle);
     sdl_handle = NULL;
     return STATUS_SUCCESS;
@@ -1022,7 +1022,7 @@ NTSTATUS WINAPI sdl_bus_init(void *args)
     return STATUS_NOT_IMPLEMENTED;
 }
 
-NTSTATUS WINAPI sdl_bus_wait(void)
+NTSTATUS WINAPI sdl_bus_wait(void *args)
 {
     WARN("SDL support not compiled in!\n");
     return STATUS_NOT_IMPLEMENTED;
